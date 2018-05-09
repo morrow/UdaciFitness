d4fa7093df2386a64e8a2b0e52da0da037932b03
= <terrymorrow@gmail.com>
2018-05-09 07:51:30 -0400

    initial commit

---
+++ components/AddEntry.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  }
+  increment = (metric)=> {
+    const { max, step } = getMetricMetaInfo(metric)
+    this.setState((state)=>{
+      const count = state[metric] + step
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      }
+    })
+  }
+  render(){
+    return (
+      <View>
+        <Text>{ getMetricMetaInfo('bike').getIcon() }</Text>
+      </View>
+    )
+  }
+}
